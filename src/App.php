dec62a239e197c7d9fd7e682169bcb85c7f897e0
刘晓彬 <563859671@qq.com>
2020-11-15 20:26:16 +0800

    版本号调整

--- src/App.php
+++ src/App.php
@@ -21,7 +21,7 @@ class App extends \think\App
      * 版本号
      * @var string
      */
-    public static $busyVersion = '3.0.3';
+    public static $busyVersion = '3.0.4';
     
     /**
      * 框架名称
