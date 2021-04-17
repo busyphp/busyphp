a52decb86d700b6bd9bc00e0369de2d30885af93
刘晓彬 <563859671@qq.com>
2021-04-17 11:06:27 +0800

    上升版本号

--- src/App.php
+++ src/App.php
@@ -21,7 +21,7 @@ class App extends \think\App
      * 版本号
      * @var string
      */
-    public static $busyVersion = '3.0.9';
+    public static $busyVersion = '3.1.0';
     
     /**
      * 框架名称
