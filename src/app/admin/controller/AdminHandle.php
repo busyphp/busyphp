764744994041ab9f283f625b8cd889ecf0517037
刘晓彬 <563859671@qq.com>
2022-09-27 16:53:58 +0800

    url 中 增加 route_path(全小写) 和 path

--- src/app/admin/controller/AdminHandle.php
+++ src/app/admin/controller/AdminHandle.php
@@ -103,7 +103,8 @@ class AdminHandle extends Handle
             'self'       => url(),
             'controller' => $request->controller(),
             'action'     => $request->action(),
-            'path'       => $request->getRoutePath(true),
+            'path'       => $request->getRoutePath(),
+            'route_path' => $request->getRoutePath(true)
         ];
         
         
@@ -111,7 +112,7 @@ class AdminHandle extends Handle
         $menuModel   = SystemMenu::init();
         $hashList    = $menuModel->getHashList();
         $breadcrumb  = [];
-        $currentMenu = $hashList[md5($data['url']['path'])] ?? null;
+        $currentMenu = $hashList[md5($data['url']['route_path'])] ?? null;
         if ($currentMenu) {
             $idList     = $menuModel->getIdList();
             $parentList = $menuModel->getIdParens();
