c1e519eb3cac74ae8d0a55bb16614c73c65c43a8
刘晓彬 <563859671@qq.com>
2023-03-23 21:22:58 +0800

    简化

--- application/admin/controller/InsideController.php
+++ application/admin/controller/InsideController.php
@@ -1,10 +1,9 @@
 <?php
+declare(strict_types = 1);
 
 namespace BusyPHP\app\admin\controller;
 
-use BusyPHP\helper\StringHelper;
-use BusyPHP\Service;
-use think\Container;
+use think\exception\HttpException;
 use think\Response;
 
 /**
@@ -12,128 +11,42 @@ use think\Response;
  * @author busy^life <busy.life@qq.com>
  * @copyright (c) 2015--2021 ShanXi Han Tuo Technology Co.,Ltd. All rights reserved.
  * @version $Id: 2020/6/4 下午6:33 下午 InsideController.php $
- * @internal
+ * @internal 仅供内部使用
  */
-class InsideController extends AdminController
+abstract class InsideController extends AdminController
 {
     /**
-     * @inheritDoc
-     */
-    protected function display($template = '', $charset = 'utf-8', $contentType = '', $content = '') : Response
-    {
-        return parent::display($this->parseTemplate($template), $charset, $contentType, $content);
-    }
-    
-    
-    /**
-     * 获取模板存放目录
-     * @return string
-     */
-    protected function getViewPath() : string
-    {
-        return dirname(__DIR__) . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR;
-    }
-    
-    
-    /**
-     * 获取当前控制器模板存放目录
-     * @return string
-     */
-    protected function getTemplatePath() : string
-    {
-        $group      = '';
-        $controller = $this->request->controller();
-        if (false !== strpos($controller, '.')) {
-            [$group, $controller] = explode('.', $controller);
-            $group = StringHelper::snake($group) . DIRECTORY_SEPARATOR;
-        }
-        $controller = StringHelper::snake($controller);
-        
-        $dir = $this->request->route(Service::ROUTE_VAR_DIR);
-        if ($dir) {
-            $dir = StringHelper::snake($dir) . DIRECTORY_SEPARATOR;
-        }
-        
-        return $this->getViewPath() . $dir . $group . $controller . DIRECTORY_SEPARATOR;
-    }
-    
-    
-    /**
-     * 解析模板地址
+     * 内置模版显示
      * @param string $template
-     * @return string
+     * @param string $charset
+     * @param string $contentType
+     * @param string $content
+     * @param array  $config
+     * @return Response
+     * @internal 仅供内部使用
      */
-    protected function parseTemplate($template = '')
+    protected function insideDisplay($template = '', $charset = 'utf-8', $contentType = '', $content = '', array $config = []) : Response
     {
-        if (0 === strpos($template, '@')) {
-            return substr($template, 1);
-        }
+        $this->app->config->set(array_merge([
+            'view_path'   => dirname(__DIR__) . '/view/',
+            'view_depr'   => DIRECTORY_SEPARATOR,
+            'view_suffix' => 'html',
+            'auto_rule'   => 1
+        ], $config), 'view');
         
-        if (!$template) {
-            return $this->getTemplatePath() . $this->request->action() . '.html';
-        } else {
-            if (is_file($template)) {
-                return $template;
-            }
-            
-            if (false === strpos($template, '/')) {
-                return $this->getTemplatePath() . $template . '.html';
-            } elseif (false === strpos($template, '.')) {
-                return $this->getViewPath() . $template . '.html';
-            } else {
-                return $template;
-            }
-        }
+        return $this->display($template, $charset, $contentType, $content);
     }
     
     
     /**
-     * 获取自定义模板属性
-     * @param string $key 自定义模板键
-     * @param string $attr 属性名称
-     * @param mixed  $default 默认值
-     * @return mixed
+     * 发行模式下禁用
+     * @param string ...$excludeAction 排除的方法
+     * @internal 仅供内部使用
      */
-    protected function getUseTemplateAttr(string $key, string $attr, $default = null)
+    protected function releaseDisabled(...$excludeAction)
     {
-        return $this->app->config->get(sprintf("app.admin.template.%s.%s", $key, $attr), $default);
-    }
-    
-    
-    /**
-     * 获取自定义模板
-     * @param string $name 自定义模板键
-     * @param string $defaultTemplate 默认魔板名称
-     * @param array  $assignVars 给模板赋值的变量
-     * @return string
-     */
-    protected function getUseTemplate(string $name, string $defaultTemplate = '', array $assignVars = []) : string
-    {
-        $template = $this->app->config->get(sprintf('app.admin.template.%s', $name), '');
-        if (is_array($template)) {
-            $assign   = $template['assign'] ?? '';
-            $template = $template['path'] ?? '';
-            $title    = $template['title'] ?? '';
-            if ($title) {
-                $this->setPageTitle($title);
-            }
-            
-            if ($assign) {
-                $assigns = (array) Container::getInstance()->invokeFunction($assign, [$assignVars]);
-                foreach ($assigns as $key => $item) {
-                    $this->assign($key, $item);
-                }
-            }
+        if (!$this->app->isDebug() && !in_array($this->request->action(), $excludeAction)) {
+            throw new HttpException(404);
         }
-        
-        foreach ($assignVars as $key => $item) {
-            $this->assign($key, $item);
-        }
-        
-        if ($template && !is_file($template)) {
-            $template = sprintf('@%s', $template);
-        }
-        
-        return $template ?: $defaultTemplate;
     }
 }
