e8d7383392f1a51a292fc275e7afd69361fd6763
刘晓彬 <563859671@qq.com>
2023-03-27 08:19:45 +0800

    返回类型

--- application/helper.php
+++ application/helper.php
@@ -5,7 +5,7 @@ if (!function_exists('ckeditor5_css_url')) {
      * 获取CkEditor5 CSS URL
      * @return string
      */
-    function ckeditor5_css_url(bool $domain = false)
+    function ckeditor5_css_url(bool $domain = false) : string
     {
         return app()->request->getAssetsUrl($domain) . 'system/css/ckeditor5.css?v=' . app()->getFrameworkVersion();
     }
@@ -16,7 +16,7 @@ if (!function_exists('ckeditor5_css_link')) {
      * 获取CkEditor5 CSS Link标签
      * @return string
      */
-    function ckeditor5_css_link(bool $domain = false)
+    function ckeditor5_css_link(bool $domain = false) : string
     {
         return '<link rel="stylesheet" href="' . ckeditor5_css_url($domain) . '"/>';
     }
